cbce39eb0d882ee3678dbdb1a55455407dcb2467
Muteen Kundangar <kundangarmuteen@gmail.com>
2026-04-08 01:33:14 +0530

    Updated client connection settings

--- frontend/src/lib/nakama/client.ts
+++ frontend/src/lib/nakama/client.ts
@@ -3,6 +3,6 @@ import { Client } from '@heroiclabs/nakama-js'
 const host = import.meta.env.VITE_NAKAMA_HOST ?? '127.0.0.1'
 const port = import.meta.env.VITE_NAKAMA_PORT ?? '7350'
 
-const client = new Client('defaultkey', host, port)
+const client = new Client('defaultkey', host, port, true)
 
 export default client
