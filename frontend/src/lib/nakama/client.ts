08280ffcfd6b3d53cf4798f2f67470ca7281624a
Muteen Kundangar <kundangarmuteen@gmail.com>
2026-04-08 00:52:06 +0530

    Updated socket connection to use env

--- frontend/src/lib/nakama/client.ts
+++ frontend/src/lib/nakama/client.ts
@@ -1,5 +1,8 @@
 import { Client } from '@heroiclabs/nakama-js'
 
-const client = new Client("defaultkey", "127.0.0.1", "7350");
+const host = import.meta.env.VITE_NAKAMA_HOST ?? '127.0.0.1'
+const port = import.meta.env.VITE_NAKAMA_PORT ?? '7350'
 
-export default client;
+const client = new Client('defaultkey', host, port)
+
+export default client
